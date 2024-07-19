226c8a3dd6a2859f7dee97481cbccd32a2b3fd55
Alan Gebhardtsbauer <alangebhardtsbauer@MacBook-Air-4.local>
2024-07-19 08:44:40 -0400

    added folke Noice

--- init.lua
+++ init.lua
@@ -280,6 +280,23 @@ require('lazy').setup({
     },
   },
 
+  -- Commandline line cmdline noice folke
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
