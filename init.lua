02cc0a2b2b941313a019b366f1d4e3e4b41225be
Alan Gebhardtsbauer <alangebhardtsbauer@MacBook-Air-4.local>
2025-07-14 13:06:55 -0400

    claude - whichkey and keybind optomizations

--- init.lua
+++ init.lua
@@ -181,15 +181,22 @@ require('lazy').setup({
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
-      -- Document existing key chains
+      -- Enhanced keybinding organization
       require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>b'] = { name = '󰓩 [B]uffer', _ = 'which_key_ignore' },
+        ['<leader>c'] = { name = '󰘦 [C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '󰈙 [D]ocument', _ = 'which_key_ignore' },
+        ['<leader>f'] = { name = '󰈞 [F]ile', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '󰊢 [G]it', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>o'] = { name = '󰏌 [O]pen', _ = 'which_key_ignore' },
+        ['<leader>p'] = { name = '󰏖 [P]roject', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = '󰍉 [S]earch', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>u'] = { name = '󰔡 [U]I', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>x'] = { name = '󰔫 [X] Diagnostics', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -35,3 +35,42 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Enhanced keybindings for better organization
+-- Buffer navigation (unbind default J and use for most recently used buffer)
+vim.keymap.set('n', 'J', '<C-^>', { desc = 'Switch to previous buffer (MRU)', silent = true })
+vim.keymap.set('n', 'L', ':bnext<CR>', { desc = 'Next buffer', silent = true })
+
+-- Buffer management
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = '[B]uffer [D]elete', silent = true })
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = '[B]uffer [N]ext', silent = true })
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { desc = '[B]uffer [P]revious', silent = true })
+vim.keymap.set('n', '<leader>ba', ':%bdelete|edit#<CR>', { desc = '[B]uffer Delete [A]ll Others', silent = true })
+vim.keymap.set('n', '<leader>bo', ':%bdelete|edit#<CR>', { desc = '[B]uffer Delete [O]thers', silent = true })
+
+-- File operations
+vim.keymap.set('n', '<leader>fs', ':w<CR>', { desc = '[F]ile [S]ave', silent = true })
+vim.keymap.set('n', '<leader>fS', ':wa<CR>', { desc = '[F]ile [S]ave All', silent = true })
+vim.keymap.set('n', '<leader>fn', ':enew<CR>', { desc = '[F]ile [N]ew', silent = true })
+vim.keymap.set('n', '<leader>fo', function() require('conform').format { async = true, lsp_fallback = true } end, { desc = '[F]ile F[o]rmat' })
+
+-- Common shortcuts
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file', silent = true })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit', silent = true })
+vim.keymap.set('n', '<leader>x', ':x<CR>', { desc = 'Save and quit', silent = true })
+
+-- Open operations
+vim.keymap.set('n', '<leader>ot', ':terminal<CR>', { desc = '[O]pen [T]erminal', silent = true })
+vim.keymap.set('n', '<leader>ol', ':Lazy<CR>', { desc = '[O]pen [L]azy', silent = true })
+vim.keymap.set('n', '<leader>om', ':Mason<CR>', { desc = '[O]pen [M]ason', silent = true })
+
+-- UI toggles
+vim.keymap.set('n', '<leader>un', ':set number!<CR>', { desc = '[U]I Toggle Line [N]umbers', silent = true })
+vim.keymap.set('n', '<leader>ur', ':set relativenumber!<CR>', { desc = '[U]I Toggle [R]elative Numbers', silent = true })
+vim.keymap.set('n', '<leader>uw', ':set wrap!<CR>', { desc = '[U]I Toggle [W]rap', silent = true })
+vim.keymap.set('n', '<leader>us', ':set spell!<CR>', { desc = '[U]I Toggle [S]pell', silent = true })
+
+-- Git operations (basic ones, gitsigns provides more)
+vim.keymap.set('n', '<leader>gs', ':!git status<CR>', { desc = '[G]it [S]tatus', silent = true })
+vim.keymap.set('n', '<leader>gl', ':!git log --oneline<CR>', { desc = '[G]it [L]og', silent = true })
+vim.keymap.set('n', '<leader>gd', ':!git diff<CR>', { desc = '[G]it [D]iff', silent = true })
