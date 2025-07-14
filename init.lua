94b8ef15fdb6e518c4dfa8cec78d8f265d45b968
Alan Gebhardtsbauer <alangebhardtsbauer@MacBook-Air-4.local>
2025-07-14 10:27:15 -0400

    working lua config... there's an error in init that i can't find?

--- init.lua
+++ init.lua
@@ -19,72 +19,15 @@
 ========                                                     ========
 =====================================================================
 =====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
   TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
 
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 --Neovide Font
 vim.o.guifont = 'JetBrainsMono Nerd Font Mono:h19'
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- TODO: Add a Cool Splash Screen
 
 -- disable netrw Netrw in favor of neo-tree
 -- vim.g.loaded_netrw = 1
@@ -96,9 +39,6 @@ vim.o.guifont = 'JetBrainsMono Nerd Font Mono:h19'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
@@ -106,8 +46,8 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
+
+-- Relative Line Numbers (in Gutter)
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
@@ -180,7 +120,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 vim.keymap.set('n', '<leader>cr', '<cmd>:cd ~/repos/<CR>', { desc = 'Change dir to [r]epo' })
 vim.keymap.set('n', '<leader>cn', '<cmd>:cd ~/.config/nvim/<CR>', { desc = 'Change dir to [n]vim config' })
 
@@ -245,6 +184,7 @@ require('lazy').setup({
   --
   --  This is equivalent to:
   --    require('Comment').setup({})
+  --
   {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
@@ -289,76 +229,88 @@ require('lazy').setup({
     },
   },
 
-  -- eyeliner install, Currently trying hop instead
+  -- Hop Install (New Version)
   -- {
-  --   'jinh0/eyeliner.nvim',
+  --   'smoka7/hop.nvim',
+  --   version = '*',
   --   config = function()
-  --     require('eyeliner').setup {
-  --       highlight_on_key = true, -- show highlights only after keypress
-  --       dim = false, -- dim all other characters if set to true (recommended!)
-  --     }
-  --   end,
-  -- },
-
-  -- Hop Install (Old, See Next Block for New Version)
-  -- {
-  --   'phaazon/hop.nvim',
-  --   branch = 'v2', -- optional but strongly recommended
-  --   config = function()
-  --     -- you can configure Hop the way you like here; see :h hop-config
   --     require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
-  --
-  --     -- Configuration options for Hop
   --     local hop = require 'hop'
   --     local directions = require('hop.hint').HintDirection
-  --     vim.keymap.set('', 'f', function()
-  --       hop.hint_words { current_line_only = false }
+  --     -- vim.keymap.set('', 'f', function()
+  --     --   hop.hint_words { current_line_only = false }
+  --     -- end, { remap = true })
+  --
+  --     vim.keymap.set('', 'ff', function()
+  --       hop.hint_words { direction = directions.AFTER_CURSOR, current_line_only = false }
   --     end, { remap = true })
-  --     -- vim.keymap.set('', 'F', function()
-  --     --   hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true }
-  --     -- end, { remap = true })
-  --     -- vim.keymap.set('', 't', function()
-  --     --   hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
-  --     -- end, { remap = true })
-  --     -- vim.keymap.set('', 'T', function()
-  --     --   hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
-  --     -- end, { remap = true })
+  --     vim.keymap.set('', 'fb', function()
+  --       hop.hint_words { direction = directions.BEFORE_CURSOR, current_line_only = false }
+  --     end, { remap = true })
+  --     vim.keymap.set('', 'FI', function()
+  --       hop.hint_lines { direction = directions.BEFORE_CURSOR, current_line_only = false }
+  --     end, { remap = true })
+  --     vim.keymap.set('', 'FK', function()
+  --       hop.hint_lines { direction = directions.AFTER_CURSOR, current_line_only = false }
+  --     end, { remap = true })
+  --     vim.keymap.set('', 't', function()
+  --       hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
+  --     end, { remap = true })
+  --     vim.keymap.set('', 'T', function()
+  --       hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
+  --     end, { remap = true })
   --   end,
   -- },
-  --
-  -- Hop Install (New Version)
+
+  -- flash
   {
-    'smoka7/hop.nvim',
-    version = '*',
-    config = function()
-      require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
-      local hop = require 'hop'
-      local directions = require('hop.hint').HintDirection
-      -- vim.keymap.set('', 'f', function()
-      --   hop.hint_words { current_line_only = false }
-      -- end, { remap = true })
-
-      vim.keymap.set('', 'ff', function()
-        hop.hint_words { direction = directions.AFTER_CURSOR, current_line_only = false }
-      end, { remap = true })
-      vim.keymap.set('', 'fb', function()
-        hop.hint_words { direction = directions.BEFORE_CURSOR, current_line_only = false }
-      end, { remap = true })
-      vim.keymap.set('', 'FI', function()
-        hop.hint_lines { direction = directions.BEFORE_CURSOR, current_line_only = false }
-      end, { remap = true })
-      vim.keymap.set('', 'FK', function()
-        hop.hint_lines { direction = directions.AFTER_CURSOR, current_line_only = false }
-      end, { remap = true })
-      vim.keymap.set('', 't', function()
-        hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
-      end, { remap = true })
-      vim.keymap.set('', 'T', function()
-        hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
-      end, { remap = true })
-    end,
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      -- {
+      --   'S',
+      --   mode = { 'n', 'x', 'o' },
+      --   function()
+      --     require('flash').treesitter()
+      --   end,
+      --   desc = 'Flash Treesitter',
+      -- },
+      -- {
+      --   'r',
+      --   mode = 'o',
+      --   function()
+      --     require('flash').remote()
+      --   end,
+      --   desc = 'Remote Flash',
+      -- },
+      -- {
+      --   'R',
+      --   mode = { 'o', 'x' },
+      --   function()
+      --     require('flash').treesitter_search()
+      --   end,
+      --   desc = 'Treesitter Search',
+      -- },
+      -- {
+      --   '<c-s>',
+      --   mode = { 'c' },
+      --   function()
+      --     require('flash').toggle()
+      --   end,
+      --   desc = 'Toggle Flash Search',
+      -- },
+    },
   },
-
   -- Commandline line cmdline noice folke
   {
     'folke/noice.nvim',
@@ -1025,7 +977,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
